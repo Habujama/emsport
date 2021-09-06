31c356cea8f3bc36905e6388043dbf8ded2108bd
hankaBurdik <hana.burdik@zonky.cz>
2021-09-06 12:03:07 +0200

    animate hero section

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,55 +1,107 @@
 import { FC } from 'react'
-import ReactArcText from 'react-arc-text-fix'
+import classNames from 'classnames'
 import { StaticImage } from 'gatsby-plugin-image'
-/* import { motion } from 'framer-motion' */
+import { motion } from 'framer-motion'
 import { useMedia } from 'react-use'
 import theme from 'tailwindcss/defaultTheme'
 
 const { screens } = theme
 
 const Hero: FC = () => {
-  const isNotMobile = useMedia(`(min-width: ${screens.lg})`)
+  const isNotMobile = useMedia(`(min-width: ${screens.md})`)
+  const imageCss = classNames('flex flex-col')
+
   return (
     <>
-      {isNotMobile ? (
-        <div className="flex">
-          <h3 className="text-blue-900 text-2xl spinner -mb-64 mx-auto">
-            <ReactArcText
-              text="Kola, lyže, servis, bajky pro všechny sporťáky."
-              direction={1}
-              arc={90}
-            />
-          </h3>
+      <div className="flex flex-col sm:flex-row justify-between">
+        <div className="flex flex-col max-w-xs justify-center">
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.7 }}
+            className="text-white text-4xl font-bold sm:pt-16"
+          >
+            Kola, servis,lyže, bajky pro&nbsp;všechny sporťáky.
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="text-white text-xl sm:mt-8 mt-4 sm:mb-16 mb-6"
+          >
+            Pro tvoje kolo máme špičkový mechaniky, pro tebe fešácký doplňky
+            a&nbsp;funkční oblečení.
+          </motion.h2>
         </div>
-      ) : null}
-      <div className="flex flex-col sm:flex-row items-center max-w-screen-lg mx-auto">
-        <StaticImage
-          src="../../assets/hero/koula-tym.png"
-          alt="emsporťák"
-          title="emsporťák"
-          height={334}
-          placeholder="blurred"
-          layout="fixed"
-          className="flex mx-auto my-8"
-        />
-        <StaticImage
-          src="../../assets/hero/tata-tym.png"
-          alt="emsporťák"
-          title="emsporťák"
-          height={282}
-          placeholder="blurred"
-          layout="fixed"
-          className="flex mx-auto mb-6 mt-14"
-        />
-        <StaticImage
-          src="../../assets/hero/tomas-tym.png"
-          alt="emsporťák"
-          title="emsporťák"
-          height={320}
-          placeholder="blurred"
-          layout="fixed"
-          className="flex mx-auto my-6"
-        />
+        {isNotMobile ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ staggerChildren: 0.5 }}
+            className="flex flex-col"
+          >
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              animate={{ opacity: 1, y: 150 }}
+              transition={{ duration: 1, delay: 0.1 }}
+            >
+              <StaticImage
+                src="../../assets/hero/emsportacka-tym.png"
+                alt="Martin Matoušek"
+                title="Martin Matoušek"
+                height={350}
+                placeholder="blurred"
+                layout="fixed"
+                className={`${imageCss} lg:-ml-12 -mt-48`}
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1, delay: 0.2 }}
+            >
+              <StaticImage
+                src="../../assets/hero/koula-tym.png"
+                alt="Martin Koula"
+                title="Martin Koula"
+                height={334}
+                placeholder="blurred"
+                layout="fixed"
+                className={`${imageCss} -mt-12 lg:-mt-16 -ml-12 lg:-ml-56`}
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              animate={{ opacity: 1, x: 150 }}
+              transition={{ duration: 1, delay: 0.1 }}
+            >
+              <StaticImage
+                src="../../assets/hero/tata-tym.png"
+                alt="Martin Matoušek"
+                title="Martin Matoušek"
+                height={282}
+                placeholder="blurred"
+                layout="fixed"
+                className={`${imageCss} -mr-56 -mt-64`}
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: -100 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              <StaticImage
+                src="../../assets/hero/tomas-tym.png"
+                alt="Tomáš Řenč"
+                title="Tomáš Řenč"
+                height={320}
+                placeholder="blurred"
+                layout="fixed"
+                className={`${imageCss} -mt-56 -ml-12 mb-8`}
+              />
+            </motion.div>
+          </motion.div>
+        ) : null}
       </div>
     </>
   )
